006dfc87dcc62ae7b769035a7a1cfd9f06504677
Richard Gichuki <chadgichuki@gmail.com>
2022-09-26 22:22:52 +0300

    Lab completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
+    # if params[:name]
+    #   name = params[:name]
+    #   students = Student.all.where(first_name: LIKE ? OR last_name: LIKE ?, "%#{name}%", "%#{name}%")
+    # else
+    #   students = Student.all
+    # end
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
